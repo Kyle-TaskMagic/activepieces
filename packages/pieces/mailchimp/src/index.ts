d239bab21d7b08faba1c7678f91884b79268e75d
kylebuilds <kyle@wilsondevelops.com>
2023-10-29 10:25:48 -0700

    inconsistent file naming/structure

--- packages/pieces/mailchimp/src/index.ts
+++ packages/pieces/mailchimp/src/index.ts
@@ -1,5 +1,5 @@
 import { PieceAuth, createPiece } from '@activepieces/pieces-framework';
-import { addMemberToList } from './lib/actions/add-member-to-list.action/add-member-to-list.action'
+import { addMemberToList } from './lib/actions/add-member-to-list.action';
 import { mailChimpSubscribeTrigger } from './lib/triggers/subscribe-trigger';
 
 export const mailchimpAuth = PieceAuth.OAuth2({
